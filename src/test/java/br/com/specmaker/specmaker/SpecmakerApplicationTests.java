d302bb8e44658dc4b5df9db74a0ba5daf7fabd22
Henrique Alle <henriquealle@gmail.com>
2022-11-12 19:14:02 -0300

    first commit - setup project

---
+++ src/test/java/br/com/specmaker/specmaker/SpecmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.specmaker.specmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecmakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
