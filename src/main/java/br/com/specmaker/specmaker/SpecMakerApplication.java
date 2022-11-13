2e1fb4345c47415b4048378c86e59f1d70506ba2
Henrique Alle <henriquealle@gmail.com>
2022-11-12 22:04:11 -0300

    fix springboot structure

---
+++ src/main/java/br/com/specmaker/specmaker/SpecMakerApplication.java
@@ -0,0 +1,13 @@
+package br.com.specmaker.specmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecMakerApplication.class, args);
+	}
+
+}
